8bb3314b94e52d41d8849a333d29d9c7d01e81ee
Andy Wermke <andy@dev.next-step-software.com>
2014-05-30 10:56:36 +0200

    Bugs fixed.

--- sample-app/app/scripts/controllers/TurtleEditorController.js
+++ sample-app/app/scripts/controllers/TurtleEditorController.js
@@ -1,10 +1,11 @@
-angular.module('app').controller('TurtleEditorController', ['$scope', 'AlertService', function($scope, AlertService) {
+angular.module('app').controller('TurtleEditorController',
+                                ['$scope', 'AlertService', function($scope, AlertService) {
 
   var editor;
 
   // Properties:
 
-  $scope.serverUrl = 'http://localhost:4000/channel';
+  $scope.serverUrl = 'http://localhost:4000/channel#test';
   $scope.turtleContents = '';
 
   $scope.editorOptions = {
@@ -21,6 +22,10 @@ angular.module('app').controller('TurtleEditorController', ['$scope', 'AlertServ
   $scope.connect = function() {
     var serverUrl = $scope.serverUrl.split('#')[0];
     var docName = $scope.serverUrl.split('#')[1];
+
+    if (!docName) {
+      return AlertService.danger('No document name given. Append to server URL as "#<doc name>".');
+    }
 
     var options = {
       // authentication: '1234567',
