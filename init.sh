67c4e77f3e99cc2c410ba95861427b3237fa5301
Andy Wermke <andy@dev.next-step-software.com>
2014-06-16 11:18:14 +0200

    Reorganized repository.

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone 'git@github.com:AKSW/sharejs-rdf.git' -b 'sharejs-rdf-ext' sharejs-rdf-ext
+git clone 'git@github.com:AKSW/sharejs-rdf.git' -b 'server' server
+git clone 'git@github.com:AKSW/sharejs-rdf.git' -b 'sample-app' sample-app
+
