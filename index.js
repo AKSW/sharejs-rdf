9365313193782f6380bafefa4abbdf96fadb1576
Andy Wermke <andy@dev.next-step-software.com>
2014-06-28 11:58:40 +0200

    Fixed a bug.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 module.exports = function (sharejs) {
   var types = require('./lib/types');
 
-  for (var i = 0; i < types.length; i++) {
-    var type = types[i];
+  for (var name in types) {
+    var type = types[name];
     sharejs.types[type.name] = type;
   }
 };
