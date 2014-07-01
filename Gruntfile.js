6cff116732731a304574d1a5c9d875cb0f6050be
Andy Wermke <andy@dev.next-step-software.com>
2014-07-01 20:22:44 +0200

    jshint now working again.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,80 +9,79 @@
 
 module.exports = function (grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-    // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    // Configurable paths
-    var config = {
-        app: 'app',
-        dist: 'dist',
-        harp: {
-            server: {
-                server: true,
-                source: 'public'
-            },
-            dist: {
-                source: 'public',
-                dest: 'build'
-            }
+  // Configurable paths
+  var config = {
+    app: 'public',
+    dist: 'build',
+    harp: {
+      server: {
+        server: true,
+        source: 'public'
+      },
+      dist: {
+        source: 'public',
+        dest: 'build'
+      }
+    }
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    harp: config.harp,
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*'
+      ]
+    },
+
+    shell: {
+      'share-server': {
+        command: 'grunt serve',
+        options: {
+          execOptions: {
+            cwd: '../server'
+          }
         }
-    };
+      }
+    },
 
-    // Define the configuration for all the tasks
-    grunt.initConfig({
+    concurrent: {
+      serve: ['serve-app', 'shell:share-server'],
+      options: {
+        logConcurrentOutput: true
+      }
+    }
 
-        // Project settings
-        config: config,
-
-        harp: config.harp,
-
-        // Make sure code styles are up to par and there are no obvious mistakes
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'Gruntfile.js',
-                '<%= config.app %>/scripts/{,*/}*.js',
-                '!<%= config.app %>/scripts/vendor/*',
-                'test/spec/{,*/}*.js'
-            ]
-        },
-
-        shell: {
-            'share-server': {
-                command: 'grunt serve',
-                options: {
-                    execOptions: {
-                        cwd: '../server'
-                    }
-                }
-            }
-        },
-
-        concurrent: {
-            serve: ['serve-app', 'shell:share-server'],
-            options: {
-                logConcurrentOutput: true
-            }
-        }
-
-    });
+  });
 
 
-    grunt.registerTask('serve-app', function () {
-        delete config.harp.dist;
-        grunt.task.run('harp');
-    });
+  grunt.registerTask('serve-app', function () {
+    delete config.harp.dist;
+    grunt.task.run('harp');
+  });
 
-    grunt.registerTask('compile', function () {
-        delete config.harp.server;
-        grunt.task.run('harp');
-    });
+  grunt.registerTask('compile', function () {
+    delete config.harp.server;
+    grunt.task.run('harp');
+  });
 
-    grunt.registerTask('default', 'concurrent:serve');
+  grunt.registerTask('default', ['jshint', 'concurrent:serve']);
 };
