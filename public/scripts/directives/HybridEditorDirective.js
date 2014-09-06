ecd5cdbea923b642199b1844d44921c9768e47ee
Andy Wermke <andy@dev.next-step-software.com>
2014-09-06 14:09:52 +0200

    Working directive logic.

--- public/scripts/directives/HybridEditorDirective.js
+++ public/scripts/directives/HybridEditorDirective.js
@@ -6,8 +6,40 @@ angular.module('app').directive('hybridEditor', function () {
     restrict: 'E',
     templateUrl: 'templates/hybrid-editor.html',
 
-    controller: ['$scope', function ($scope) {
-      // TODO
+    controller: ['$scope', 'AlertService', 'RdfJsonAttachmentService',
+                function ($scope, AlertService, RdfJsonAttachmentService) {
+      
+      var turtleEditor;
+
+      $scope.codemirrorLoaded = function (_editor) {
+        turtleEditor = _editor;
+      };
+
+      $scope.connect = function () {
+        var serverUrl = $scope.serverUrl.split('#')[0];
+        var docName = $scope.serverUrl.split('#')[1];
+
+        if (!docName) {
+          return AlertService.danger('No document name given. Append to server URL as "#<doc name>".');
+        }
+
+        var options = {
+          // authentication: '1234567',
+          origin: serverUrl
+        };
+
+        sharejs.open(docName, 'turtle-rdf-json', options, function (error, doc) {
+          if (error) {
+            return AlertService.danger(error);
+          }
+
+          doc.attach_cm(turtleEditor);
+          RdfJsonAttachmentService.attachDocToEditor(doc, $scope);
+
+          AlertService.success('Connected to ' + serverUrl + '#' + docName);
+        });
+      };
+
     }]
   };
 });
