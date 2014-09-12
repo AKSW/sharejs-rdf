61c02f29c5f1d26d19251c692ba47d22981d5f26
Andy Wermke <andy@dev.next-step-software.com>
2014-09-12 16:16:35 +0200

    Cleaned code.

--- public/scripts/controllers/HybridEditorController.js
+++ public/scripts/controllers/HybridEditorController.js
@@ -1,17 +1,11 @@
 angular.module('app').controller('HybridEditorController',
-                                ['$scope', '$element', 'AlertService', function ($scope, $element, AlertService) {
+                                ['$scope', function ($scope) {
   'use strict';
 
   // Properties:
 
   $scope.serverUrl = 'http://' + document.location.hostname + ':4000/channel#test';
-  $scope.jsonRdfContents = {};
 
-
-  var shareDoc;
-
-  $scope.connect = function () {
-    // TODO
-  };
+  // The logic is placed in the HybridEditorDirective
 
 }]);
