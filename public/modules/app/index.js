38366a2eef274b72a6be2f26f8cf35a7521b1f14
Andy Wermke <andy@dev.next-step-software.com>
2014-10-13 20:35:44 +0200

    Now configuring namespaces.

--- public/modules/app/index.js
+++ public/modules/app/index.js
@@ -1,1 +1,7 @@
-angular.module('app', ['ui.bootstrap', 'ui.codemirror', 'rdfshare']);
+angular.module('app', ['ui.bootstrap', 'ui.codemirror', 'rdfshare'])
+  .run(['Namespaces', function(Namespaces) {
+    'use strict';
+
+    Namespaces.setNamespace('ex', 'http://example.com/ontology/');
+  }]
+);
