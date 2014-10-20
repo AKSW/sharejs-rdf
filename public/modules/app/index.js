87b95eb081b8ad826d6402ce1f1a619f740bb4c5
Andy Wermke <andy@dev.next-step-software.com>
2014-10-20 19:10:28 +0200

    Namespace API change.

--- public/modules/app/index.js
+++ public/modules/app/index.js
@@ -2,6 +2,8 @@ angular.module('app', ['ui.bootstrap', 'ui.codemirror', 'rdfshare'])
   .run(['Namespaces', function(Namespaces) {
     'use strict';
 
-    Namespaces.setNamespace('ex', 'http://example.com/ontology/');
+    Namespaces.register({
+      'ex': 'http://example.com/ontology/'
+    });
   }]
 );
